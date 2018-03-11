cb6c9e114cb2435721670dc1a3425acb727e3c0b
Egor Rudinsky <rudgv2008@gmail.com>
2018-03-11 08:14:31 +0300

    added some style

--- template.js
+++ template.js
@@ -1,5 +1,7 @@
 const {EOL} = require('os')
 
+const css = `:root{--primary-back:#f4f5f6;--second-back:#e1e1e1;--primary:#606c76;--link:#547794;--visited:#294f6e;--hover:#769bb9}*{padding:0;margin:0;box-sizing:border-box}body{width:100vw;min-height:100vh;font-family:Cambria,Cochin,Georgia,Times,'Times New Roman',serif;background-color:var(--primary-back);color:var(--primary);padding:5vh 7vw}h1{line-height:1.8em}h2{line-height:1.6em}h3{line-height:1.4em}ul{list-style-type: circle;margin:2vh 0;padding:1vh 2vw;background-color:var(--second-back)}li{font-size:1.1em;line-height:1.2em;margin:1vh 0}a{color:var(--link)}a:visited{color:var(--visited)}a:hover{color:var(--hover)}`
+
 const header = (directory_name, {title, desc}) => 
   (
     [title, desc, `Index of ${directory_name}:`]
@@ -14,6 +16,7 @@ const index = (directory_name, meta, links) =>
   <meta charset="utf-8">
   <title>${meta.title ? meta.title : directory_name}</title>
   <style>
+    ${css}
   </style>
 </head>
 ${
